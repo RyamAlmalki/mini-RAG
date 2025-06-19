7aff57e79a154a577d724190a17487690f0ea9dc
ryammalki <ryam.malki@gmail.com>
2025-06-19 11:16:44 +0300

    Add PgVector enums for table schema and distance methods

--- src/stores/vectordb/VectorDBEnum.py
+++ src/stores/vectordb/VectorDBEnum.py
@@ -2,8 +2,23 @@ from enum import Enum
 
 class VectorDBEnum(Enum):
     QDRANT = "qdrant"
+    PGVECTOR = "pgvector"
 
 
 class DistanceMethodEnum(Enum):
     COSINE = "Cosine"
     DOT = "Dot"
+
+
+class PgVectorTableSchemaEnum(Enum):
+    ID = "id"
+    TEXT = "text"
+    VECTOR = "vector"
+    CHUNK_ID = "chunk_id"
+    METADATA = "metadata"
+    _PREFIX = "pgvector"
+
+class PgVectorDistanceMethodEnum(Enum):
+    COSINE = "vector_cosine_ops"
+    DOT = "vector_12_ops"
+    
